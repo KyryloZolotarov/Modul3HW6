e93e6faa4a0cb2585bfc7487741942a783e6e229
Kyrylo Zolotarov <zlatashay@gmail.com>
2023-05-14 21:23:27 +0300

    done!

---
+++ Modul3HW6/Modul3HW6/MessageBox.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modul3HW6
+{
+    internal class MessageBox
+    {
+        public MessageBox()
+        {
+            this.WindowClosedEvent += StateResult;
+        }
+
+        public delegate void WindowClosed(State state);
+        public event WindowClosed WindowClosedEvent;
+        public void StateResult(State state)
+        {
+            if (state == State.Cansel)
+            {
+                Console.WriteLine("Operation denied");
+            }
+            else
+            {
+                Console.WriteLine("Operation confirmed");
+            }
+        }
+
+        public async Task Open()
+        {
+            Console.WriteLine("Window open");
+            await Task.Delay(3000);
+            Console.WriteLine("Window closed by user");
+            await ResultInvoke();
+        }
+
+        public Task ResultInvoke()
+        {
+            var rand = new Random().Next(0, 2);
+            if (rand == 0)
+            {
+                WindowClosedEvent.DynamicInvoke(State.Cansel);
+            }
+            else
+            {
+                WindowClosedEvent.DynamicInvoke(State.Ok);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
